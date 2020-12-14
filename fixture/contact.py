c2c7ba093ba2b192b1ceb612b349d877b7a9040b
akomissarova <akomissarovaaaa@gmail.com>
2020-12-14 19:47:10 +0100

    Added test for editing the first contact in the list Improved locators in contact.create and contact.delete_first_contact Deleted unnecessary clicks on contact.create

--- fixture/contact.py
+++ fixture/contact.py
@@ -11,12 +11,10 @@ class ContactHelper:
         # open contact creation form
         wd.find_element(By.LINK_TEXT, "add new").click()
         # fill in form
-        wd.find_element(By.NAME, "firstname").click()
         wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
-        wd.find_element(By.NAME, "mobile").click()
         wd.find_element(By.NAME, "mobile").send_keys(contact.mobile)
         # submit form
-        wd.find_element(By.CSS_SELECTOR, "input:nth-child(87)").click()
+        wd.find_element(By.NAME, "submit").click()
         self.return_to_home_page()
 
     def return_to_home_page(self):
@@ -25,8 +23,15 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        wd.find_element(By.NAME, 'selected[]').click()
-        wd.find_element(By.CSS_SELECTOR, ".left:nth-child(8) > input").click()
-        # assert self.driver.switch_to.alert.text == "Delete 1 addresses?"
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.XPATH, "//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
+        self.return_to_home_page()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element(By.XPATH, "//img[@alt=\'Edit\']").click()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile)
+        wd.find_element(By.NAME, "update").click()
         self.return_to_home_page()
