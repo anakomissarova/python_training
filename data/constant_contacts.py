8b0acce3857668c600d405ed3c31f1785aa7b0e6
akomissarova <akomissarovaaaa@gmail.com>
2021-01-09 19:34:50 +0100

    Parametrized test data for test_edit_contact. Data is read from a Python module.

---
+++ data/constant_contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(lastname="", middlename="", firstname=""),
+    Contact(lastname="new lastname", firstname="", mobile="+7 922 132 44 55"),
+    Contact(lastname="", firstname="new firstname", secondary="(123)45-33-11")
+]
--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -2,12 +2,12 @@ from model.contact import Contact
 from random import randrange
 
 
-def test_edit_contact(app):
+def test_edit_contact(app, data_constant_contacts):
     if not app.contact.count():
         app.contact.create(Contact(firstname="tmp"))
     old_contacts = app.contact.get_contacts_list()
     index = randrange(len(old_contacts))
-    new_contact = Contact(firstname="Firstname 1", lastname="Lastname")
+    new_contact = data_constant_contacts
     new_contact.contact_id = old_contacts[index].contact_id
     app.contact.edit_contact_by_index(index, new_contact)
     assert len(old_contacts) == app.contact.count()
