55de8e1f43ee54ed4486e7db864aa8ede4b9bc30
akomissarova <akomissarovaaaa@gmail.com>
2021-01-09 18:47:32 +0100

    Parametrized test data for test_edit_group

--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -2,12 +2,12 @@ from model.group import Group
 from random import randrange
 
 
-def test_edit_group_name(app):
+def test_edit_group_constant_data(app, json_constant_groups_to_edit):
     if not app.group.count():
         app.group.create(Group(name="tmp"))
     old_groups = app.group.get_groups_list()
     index = randrange(len(old_groups))
-    new_group = Group(name='new name')
+    new_group = json_constant_groups_to_edit
     new_group.group_id = old_groups[index].group_id
     app.group.edit_group_by_index(index, new_group)
     assert len(old_groups) == app.group.count()
@@ -16,10 +16,15 @@ def test_edit_group_name(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_edit_group_header(app):
+def test_edit_group_random_data(app, data_random_groups):
     if not app.group.count():
         app.group.create(Group(name="tmp"))
     old_groups = app.group.get_groups_list()
-    app.group.edit_first_group(Group(header='new header'))
+    index = randrange(len(old_groups))
+    new_group = data_random_groups
+    new_group.group_id = old_groups[index].group_id
+    app.group.edit_group_by_index(index, new_group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_groups_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[index] = new_group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
